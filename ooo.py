bd0938ffcde0ec3b0923e771da659ed897f8bec5
adam deeley <ad.deeley@gmail.com>
2016-11-02 09:51:44 +0000

    Removed the duplicate functions for each operand and replaced them with logic in the 'run' function.

--- ooo.py
+++ ooo.py
@@ -4,11 +4,11 @@ coded by Adam Deeley
 
 to do:
 1. add iterators to recursive functions
-2. clean up logic
 3 round to 2 dp
 4 call float once on the list instead of each function call
 ------------------------------------"""
-
+# variables:
+decimal_places = 3 
 
 # takes a string from the user and splits this into a list
 equation = input("Please input the equation you would like to step through. \n")
@@ -18,140 +18,61 @@ eq_list = equation.split(" ")
 # checks for duplicate spaces
 xs_spaces = []
 
-def list_parse(eq_list):
-    xNew = []
-    for n in range(0, len(eq_list)):
-        if xNew == []:
-            xNew.append("")
-        elif eq_list[n] == " ":
-            continue
-        elif eq_list[n] in ("1","2","3","4","5","6","7","8","9","0"):
-            xNew[-1] = xNew[-1] + eq_list[n]
-        else:
-            xNew.append(eq_list[n])
-            xNew.append("")    
-            return xNew
-           
-    for n in xNew:
-        try:
-            int(xNew[n]) # fix this somehow
-        except ValueError:
-            continue
-  
-#eq_list = list_parse(eq_list) 
-
 step = 0
 
 def find_operands(eq_list, step):
-    if "**" in eq_list and len(eq_list) > 1:
-        int_run(eq_list, step)
-    elif "/" in eq_list and len(eq_list) > 1:
-        div_run(eq_list, step)
-    elif "*" in eq_list and len(eq_list) > 1:
-        multi_run(eq_list, step)
-    elif "+" in eq_list and len(eq_list) > 1:
-        plus_run(eq_list, step)
-    elif "-" in eq_list and len(eq_list) > 1:
-        minus_run(eq_list, step)
+    if "**" in eq_list or "/" in eq_list or "*" in eq_list or "+" in eq_list or "-" in eq_list and len(eq_list) > 1:
+        run(eq_list, step)
     else:
         print("Done")
 
-def int_run(eq_list, step):
+def run(eq_list, step):
     """loop over the list and add or subtract the numbers,  then add back to list and 
         remove the adjacent values"""
     step += 1
     n = None
     for n in range(len(eq_list)):
-        if eq_list[n] == "**":
-            n = n
-            print("\nStep %s:" % step)
-            print(eq_list[n-1], " ** ", eq_list[n+1])
-            temp = float(eq_list[n-1]) ** float(eq_list[n+1]) # holds the equated value   
+        if eq_list[n] in ["**", "/", "*", "+", "-"]:
+            if eq_list[n] == "**":
+                n = n
+                print("\nStep %s:" % step)
+                print(eq_list[n-1], " ** ", eq_list[n+1])
+                temp = float(eq_list[n-1]) ** float(eq_list[n+1])
+            elif eq_list[n] == "/":
+                n = n
+                print("\nStep %s:" % step)
+                print(eq_list[n-1], " / ", eq_list[n+1])
+                temp = round(float(eq_list[n-1]) / float(eq_list[n+1]))
+            elif eq_list[n] == "*":
+                n = n
+                print("\nStep %s:" % step)
+                print(eq_list[n-1], " * ", eq_list[n+1])
+                temp = float(eq_list[n-1]) * float(eq_list[n+1])         
+            elif eq_list[n] == "+":
+                n = n
+                print("\nStep %s:" % step)
+                print(eq_list[n-1], " + ", eq_list[n+1])
+                temp = float(eq_list[n-1]) + float(eq_list[n+1])
+            elif eq_list[n] == "-":
+                n = n
+                print("\nStep %s:" % step)
+                print(eq_list[n-1], " - ", eq_list[n+1])
+                temp = float(eq_list[n-1]) - float(eq_list[n+1])                 
             eq_list[n] = temp # inserts temp into the list and removes the used numbers
             del eq_list[n+1]
             del eq_list[n-1]
-            print("Updated equation: ", eq_list)
+            
+            string_eq = ""
+            for n in eq_list: # makes a string representation of eq_list
+                string_eq += str(n) + " "
+                
+            print("Updated equation: ", string_eq)
             break
     
     # checks if the operator is still in eq_list and calls the run again. Moves onto the 
     # next operator in bidmas if not.
     find_operands(eq_list, step)
 
-def div_run(eq_list, step):
-    """loop over the list and add or subtract the numbers,  then add back to list and 
-        remove the adjacent values"""
-    step += 1
-    n = None
-    for n in range(len(eq_list)):
-        if eq_list[n] == "/":
-            n = n
-            print("\nStep %s:" % step)
-            print(eq_list[n-1], " / ", eq_list[n+1])
-            temp = float(eq_list[n-1]) / float(eq_list[n+1]) # holds the equated value   
-            eq_list[n] = temp 
-            del eq_list[n+1]
-            del eq_list[n-1]
-            print("Updated equation: ", eq_list)
-            break
-    
-    find_operands(eq_list, step)
-        
-def multi_run(eq_list, step):
-    """loop over the list and add or subtract the numbers,  then add back to list and 
-        remove the adjacent values"""
-    step += 1
-    n = None
-    for n in range(len(eq_list)):
-        if eq_list[n] == "*":
-            n = n
-            print("\nStep %s:" % step)
-            print(eq_list[n-1], " * ", eq_list[n+1])
-            temp = int(eq_list[n-1]) * int(eq_list[n+1])
-            eq_list[n] = temp
-            del eq_list[n+1]
-            del eq_list[n-1]
-            print("Updated equation: ", eq_list)
-            break
-    
-    find_operands(eq_list, step)
-        
-def plus_run(eq_list, step):
-    """loop over the list and add or subtract the numbers,  then add back to list and 
-        remove the adjacent values"""
-    step += 1
-    n = None
-    for n in range(len(eq_list)):
-        if eq_list[n] == "+":
-            n = n
-            print("\nStep %s:" % step)
-            print(eq_list[n-1], " + ", eq_list[n+1])
-            temp = int(eq_list[n-1]) + int(eq_list[n+1])
-            eq_list[n] = temp
-            del eq_list[n+1]
-            del eq_list[n-1]
-            print("Updated equation: ", eq_list)
-            break
-    
-    find_operands(eq_list, step)
-    
-def minus_run(eq_list, step):
-    """loop over the list and add or subtract the numbers,  then add back to list and 
-        remove the adjacent values"""
-    step += 1
-    n = None
-    for n in range(len(eq_list)):
-        if eq_list[n] == "-":
-            n = n
-            print("\nStep %s:" % step)
-            print(eq_list[n-1], " - ", eq_list[n+1])  
-            temp = int(eq_list[n-1]) - int(eq_list[n+1])
-            eq_list[n] = temp
-            del eq_list[n+1]
-            del eq_list[n-1]
-            print("Updated equation: ", eq_list)
-            break
-    
-    find_operands(eq_list, step)     
         
 find_operands(eq_list, step)   # calls the highest function from bidmas given - only 'dmas ATM
    
